67fdd400820f8e65bbcc0f7f2c257941939fb376
Sjouke Yntema <suyntema@live.nl>
2019-01-25 23:51:56 +0100

    Start Character Controller
    Add:
    - Start generic Character Controller

---
+++ Assets/Scripts/Character Movement/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿namespace Sjouke.Controls
+{
+    using UnityEngine;
+
+    [RequireComponent(typeof(Rigidbody))]
+    public sealed class CharacterController : MonoBehaviour
+    {
+        private Rigidbody _rb;
+
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+#endif
+
+        private void Start()
+        {
+            if (_rb == null) 
+                _rb = GetComponent<Rigidbody>();
+        }
+    }
+}
