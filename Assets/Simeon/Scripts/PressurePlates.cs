d0939e02b65a888588e96df43d35dc6dfff2ae8e
Simeon <simeon2009@live.nl>
2019-01-26 16:04:29 +0100

    Stuff

---
+++ Assets/Simeon/Scripts/PressurePlates.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PressurePlates : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Penguin_Movement>()) col.GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up, col.transform.position, ForceMode.Force);
+    }
+}
