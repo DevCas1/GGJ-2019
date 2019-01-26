62699f9b5b7d5b9e6e9d97167e7cb44d2473a48e
Sjouke Yntema <suyntema@live.nl>
2019-01-26 15:49:06 +0100

    Import Polarbear models | import audio

--- Assets/Scripts/Character Movement/PlayerController.cs
+++ Assets/Scripts/Character Movement/PlayerController.cs
@@ -3,15 +3,29 @@
     using UnityEngine;
     using CodeArchitecture.Variables;
 
+    [CreateAssetMenu(fileName = "General_Character_Settings", menuName = "Character Settings/General Character Settings")]
+    public class CharacterSettings : ScriptableObject
+    {
+        [Header("General")]
+        public float MovementSpeed;
+        [Space(10), Header("Jumping")]
+        public float JumpForce;
+
+        public float JumpFallSubstraction;
+        public float JumpCheckDistance;
+        public Vector3 JumpCheckOffset;
+    }
+
     [RequireComponent(typeof(Rigidbody))]
-    public class PlayerController : MonoBehaviour
+    public abstract class PlayerController : MonoBehaviour
     {
         [Header("Inputs (REQUIRED)")]
         public CharacterSettings CharacterSettings;
         public InputVariable MovementInput;
-        public BoolVariable SpacebarVariable;
+        public BoolVariable JumpButtonVariable;
 
         protected Rigidbody _rb;
+        protected Vector3 _currentVelocity;
         protected RaycastHit[] _raycastBuffer = new RaycastHit[5];
         private bool _isJumping = false;
         private float _currentJumpVelocity;
@@ -31,11 +45,16 @@
 
         protected virtual void Update()
         {
-            Vector3 newFrameVelocity = Vector3.zero;
+            PerformMovement();
+        }
+
+        protected virtual void PerformMovement()
+        {
+            _currentVelocity = Vector3.zero;
 
             if (Physics.RaycastNonAlloc(transform.position + CharacterSettings.JumpCheckOffset, -transform.up, _raycastBuffer, CharacterSettings.JumpCheckDistance) > 0)
             {
-                if (SpacebarVariable.Value)
+                if (JumpButtonVariable.Value)
                 {
                     _isJumping = true;
                     _currentJumpVelocity = CharacterSettings.JumpForce;
@@ -51,15 +70,15 @@
                 _currentJumpVelocity -= CharacterSettings.JumpFallSubstraction;
 
             float forwardMotion = Mathf.Abs(MovementInput.Value.x) > 0.05 ? MovementInput.Value.x : 0;
-            newFrameVelocity = new Vector3(forwardMotion * CharacterSettings.MovementSpeed, _currentJumpVelocity, 0);
+            _currentVelocity = new Vector3(forwardMotion * CharacterSettings.MovementSpeed, _currentJumpVelocity, 0);
 
             if (Mathf.Abs(MovementInput.Value.x) > 0.05)
             {
-                var currentRotation = transform.rotation.eulerAngles;
-                transform.rotation = Quaternion.Euler(0, forwardMotion > 0 ? 0 : 180, currentRotation.z);
+                var currentRotation = _rb.rotation.eulerAngles;
+                _rb.rotation = Quaternion.Euler(0, forwardMotion > 0 ? 0 : 180, currentRotation.z);
             }
 
-            _rb.MovePosition(_rb.position + newFrameVelocity * Time.deltaTime);
+            _rb.MovePosition(_rb.position + _currentVelocity * Time.deltaTime);
         }
     }
 }
